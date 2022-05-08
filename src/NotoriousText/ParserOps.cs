bdce898ad5915ef916ed012e5b5aa53b7264b7cf
Garrett van Wageningen <ghvanw283@gmail.com>
2022-05-08 00:46:13 -0500

    Update ParserOps.cs

--- src/NotoriousText/ParserOps.cs
+++ src/NotoriousText/ParserOps.cs
@@ -3,5 +3,5 @@ namespace NotoriousText;
 public static class ParserOps {
 
     public static Parser<B> Select<A, B>(this Parser<A> parser, Func<A, B> fn) =>
-        new Map(parser, fn);
+        new Map<A, B>(parser, fn);
 }
